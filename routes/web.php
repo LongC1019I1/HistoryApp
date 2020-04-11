59cfabffd7136a823f682eb5ad6d792b6aad8c7a
BachLong <bachthanhlong.it@gmail.com>
2020-04-11 16:35:29 +0700

    api

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','MoviesController@index' )->name('movies.index');
+Route::get('/movies/{movie}','MoviesController@show' )->name('movies.show');
 
 
-Route::view('/','index');
-Route::view('/movie','show');
+
